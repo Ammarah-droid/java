5a37f80bf64b7ccc17950119786c6ad8f204196e
Ammarah-droid <ammarahmo2410@gmail.com>
2025-12-12 13:29:08 +0300

    add initial file

---
+++ MAIN.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MAIN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+       
+        String name=scanner.nextLine();
+        System.out.print("Enter your age: ");
+
+
+        int age=scanner.nextInt();
+        System.out.println("-----");
+        System.out.println("Hello,"+name);
+        System.out.println("you are,"+age+"years");
+        scanner.close();
+    
+
+
+
+
+    }
+}
